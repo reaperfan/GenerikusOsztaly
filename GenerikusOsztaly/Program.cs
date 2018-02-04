c0e879cea6a5688d4ac36b4e2802d6fa62c900d8
Reaperfan <Reaperfanz@yahoo.com>
2018-02-04 12:20:31 +0100

    struct version

--- GenerikusOsztaly/Program.cs
+++ GenerikusOsztaly/Program.cs
@@ -10,6 +10,50 @@ namespace GenerikusOsztaly
     {
         static void Main(string[] args)
         {
+        
+            Pont<int> p = new Pont<int>(5,5);
+            Console.WriteLine(p.ToString());
+            p.Def();
+            Console.WriteLine(p.ToString());
+            Console.ReadKey();
         }
+
+    }
+
+    internal class Pont<T> where T : struct
+    {
+        private T x;
+
+        public T MyX
+        {
+            get { return x; }
+            set { x = value; }
+            
+       }
+
+
+        private T y;
+       
+        public T MyY
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        public Pont(T x, T y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public override string ToString()
+        {
+            return "X: " + x + "\nY: " + y;
+        }
+        public void Def()
+        {
+            this.x = default(T);
+            this.y = default(T);
+        }
+
+
     }
 }
